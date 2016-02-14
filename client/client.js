6bb2c4150ab3ea0c90afa01554a343148f488b3c
Nina V <nvolkmut@uci.edu>
2016-02-14 09:32:34 -0800

    Added search function

--- client/client.js
+++ client/client.js
@@ -6,6 +6,11 @@ function halp() {
 	randomRecipe.set(Recipes.find({"key": randNum()}));
 }
 
+function search(isVege, isVegan, isGlutenFree, isDairyFree) {
+	var temp = true;
+	return Recipes.find({vegetarian : {$in : [temp, isVege]}, vegan : {$in : [temp, isVegan]}, glutenFree : {$in : [temp, isGlutenFree]}, dairyFree : {$in : [temp, isDairyFree]}, key : {$lte : parseInt((Math.random() * Recipes.find().count()), 10)}});
+}
+
 /* need a search function */
 if (Meteor.isClient) {	
 var randomRecipe = new ReactiveVar()
@@ -25,13 +30,39 @@ var randomRecipe = new ReactiveVar()
   
   Template.nomrecipe.events({
 	  "click .gimme": function(event) {
+		  console.log(search(true, false, false, false));
 		  halp();
 	  }
   });
   
+  
+    Template.recipe.helpers({
+    steps: function (sobs) {
+       if(sobs.steps != null) {
+		   return sobs.steps;
+	   }
+	   else {
+		   return 'Gotta go here: <a href="' + sobs.source + '">here.</a>';
+	   }
+    }
+  });
+  
   Template.index.events({
   	  "click .gimme": function(event) {
+		var a, b, c, d;
+		a = $("#veg").is(':checked');
+		b = $("#vegan").is(':checked');
+		c = $("#gluten").is(':checked');
+		d = $("#dairy").is(':checked');
+		console.log(a + " " + b + " " + c + " " + d  + " ");
+		if(a || b || c || d) {
+			var ahh = search(a, b, c, d);
+			console.log(ahh);
+			randomRecipe.set(ahh);
+		}
+		else {
 		  halp();
+	    }
 	  }
   });
  
